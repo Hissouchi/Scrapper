8fff7f033e8d8c72e5461732538fcede9bb05bc8
Hasnaa ouchitachen <ouchitachen.hasnaa@gmail.com>
2024-12-12 19:44:35 +0100

    create env and correct some bugs

--- scrapper/api/views.py
+++ scrapper/api/views.py
@@ -13,6 +13,7 @@ class Scrapper(APIView):
     def fetch_sitemap_urls(self, website_url):
         try:
             robots_url = f"{website_url}/robots.txt"
+            print(robots_url)
             response = requests.get(robots_url, headers=self.headers)
             sitemap_urls = []
             if response.status_code == 200:
@@ -29,28 +30,40 @@ class Scrapper(APIView):
         try:
             # Check common pagination patterns and limit to a maximum of 50 pages
             paginated_urls = []
-            for i in range(1, 51):  # Limit to 50 pages maximum
-                paginated_url = re.sub(r'(page=|/page/)(\d+)', rf'\g<1>{i}', url)
-                paginated_urls.append(paginated_url)
+            if re.search(r'(page=|/page/)', url):
+                for i in range(1, 51):  # Limit to 50 pages maximum
+                    paginated_url = re.sub(r'(page=|/page/)(\d+)', rf'\g<1>{i}', url)
+                    print(paginated_url)
+                    paginated_urls.append(paginated_url)
+            else :
+                paginated_urls.append(url) 
+                print(paginated_urls)
+                
             return paginated_urls
         except Exception as e:
             print(f"Failed to detect pagination: {e}")
             return []
 
-    def extract_content(self, url, content_type):
+    def extract_content(self, url, content_types):
         try:
             response = requests.get(url, headers=self.headers)
-            if response.status_code == 200:
+            if response.status_code == 200 :
                 soup = BeautifulSoup(response.text, 'html.parser')
-                if content_type == "titles":
-                    return [tag.text for tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])]
-                elif content_type == "links":
-                    return [a['href'] for a in soup.find_all('a', href=True)]
-                elif content_type == "paragraphs":
-                    return [p.text for p in soup.find_all('p')]
-                elif content_type == "tables":
-                    return [str(table) for table in soup.find_all('table')]
-            return []
+                datas_extracted = []
+                for content_type in content_types :
+                    if content_type == "titles":
+                        datas_extracted.extend(["titles",url])
+                        datas_extracted.append([tag.text for tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])]) 
+                    elif content_type == "links":
+                        datas_extracted.extend(["links",url])
+                        datas_extracted.append([a['href'] for a in soup.find_all('a', href=True)])
+                    elif content_type == "paragraphs":
+                        datas_extracted.extend(["paragraphs",url])
+                        datas_extracted.append([p.text for p in soup.find_all('p')])
+                    elif content_type == "tables":
+                        datas_extracted.extend(["tables",url])
+                        datas_extracted.append([str(table) for table in soup.find_all('table')])
+            return datas_extracted
         except Exception as e:
             print(f"Failed to extract content from {url}: {e}")
             return []
@@ -58,10 +71,11 @@ class Scrapper(APIView):
     def post(self, request):
         urls = request.data.get("urls", [])
         content_type = request.data.get("content_type")
-        is_full_website = request.data.get("full_website", False)
+        is_full_website = request.data.get("full_website", True)
         
         extracted_data = []
         try:
+            #print('Is_full'+len(urls))
             if is_full_website and len(urls) == 1:
                 website_url = urls[0]
                 sitemap_urls = self.fetch_sitemap_urls(website_url)
